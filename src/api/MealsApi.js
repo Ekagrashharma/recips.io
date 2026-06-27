2a8cb3cd8e929c0d2cdacad6dc9194945d3dfb13
ekagra <ekagrashharma@gmail.com>
2026-06-27 12:09:33 +0530

    refactor: remove App.css and implement meal fetching logic in App component

---
+++ src/api/MealsApi.js
@@ -0,0 +1,18 @@
+const BASE_URL = "https://www.themealdb.com/api/json/v1/1";
+//https://www.themealdb.com/api/json/v1/1/search.php?s=chicken for meals 
+//https://www.themealdb.com/api/json/v1/1/categories.php for categories list 
+
+
+export async function searchMeals(query) {
+  const response = await fetch(
+    `${BASE_URL}/search.php?s=${encodeURIComponent(query)}`
+  );
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch meals");
+  }
+
+  const data = await response.json();
+
+  return data;
+}
